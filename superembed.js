fd654d26e8ffb2b62910014fa74911df390e39d1
Corbin Davenport <me@corbin.cc>
2016-08-11 18:14:28 -0400

    Upload SuperEmbed 2.0

--- superembed.js
+++ superembed.js
@@ -6,27 +6,52 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 */
 
-// Docready.js from https://github.com/jfriend00/docReady
-!function(t,e){"use strict";function n(){if(!a){a=!0;for(var t=0;t<o.length;t++)o[t].fn.call(window,o[t].ctx);o=[]}}function d(){"complete"===document.readyState&&n()}t=t||"docReady",e=e||window;var o=[],a=!1,c=!1;e[t]=function(t,e){return a?void setTimeout(function(){t(e)},1):(o.push({fn:t,ctx:e}),void("complete"===document.readyState||!document.attachEvent&&"interactive"===document.readyState?setTimeout(n,1):c||(document.addEventListener?(document.addEventListener("DOMContentLoaded",n,!1),window.addEventListener("load",n,!1)):(document.attachEvent("onreadystatechange",d),window.attachEvent("onload",n)),c=!0)))}}("docReady",window);
-
-// Search for video embeds at page ready and modify them
-docReady(function() {
-	// Supports YouTube, Vimeo, Kickstarter, CollegeHumor, Hulu, Flickr, Vine, VideoPress, DailyMotion, Vid.Me, Twitch.tv, and all other elements with superembed-force class
+// Search for video embeds and modify them
+function resizeVids() {
 	var embeds = document.body.querySelectorAll("iframe[src*='//www.youtube.com/embed'],iframe[src*='//player.vimeo.com/video'],iframe[src*='//www.kickstarter.com/projects'],iframe[src*='//players.brightcove.net/'],iframe[src*='//www.hulu.com/embed'],object[data*='//www.flickr.com/apps/video'],iframe[src*='//vine.co/v/'],iframe[src*='//videopress.com/embed'],iframe[src*='//www.dailymotion.com/embed'],iframe[src*='//vid.me/e/'],iframe[src*='//player.twitch.tv/'],.superembed-force");
 
 	[].forEach.call(embeds, function(iframe) {
 		if (!(iframe.classList.contains("superembed-ignore"))) {
-			// Add parent element
-			var parent = iframe.parentNode;
-			var wrapper = document.createElement('div');
-			if ((iframe.classList.contains("superembed-square")) || (iframe.src.includes("//vine.co/v/"))) {
-				wrapper.setAttribute("style", "position: relative; padding-bottom: 95%; /* 4:3 */ padding-top: 25px; height: 0;");
-			} else {
-				wrapper.setAttribute("style", "position: relative; padding-bottom: 53%; /* 16:9 */ padding-top: 25px; height: 0;");
+			// Original aspect ratio is kept in data attribute to maintain scaling
+			if (!(iframe.hasAttribute("data-width"))) {
+				if (iframe.classList.contains("superembed-square")) {
+					iframe.setAttribute("data-width", "1");
+					iframe.setAttribute("data-height", "1");
+				} else if ((iframe.hasAttribute("width")) && (iframe.hasAttribute("height"))) {
+					iframe.setAttribute("data-width", iframe.offsetWidth);
+					iframe.setAttribute("data-height", iframe.offsetHeight);
+				} else {
+					// Use 16:9 ratio if element doesn't have specified width and height
+					iframe.setAttribute("data-width", "16");
+					iframe.setAttribute("data-height", "9");
+				}
 			}
-			parent.replaceChild(wrapper, iframe);
-			wrapper.appendChild(iframe);
-			iframe.setAttribute("style", "position: absolute; top: 0; left: 0; width: 100%; height: 100%;");
+			// Resizing code based on http://stackoverflow.com/a/3971875
+			var maxWidth = iframe.parentElement.offsetWidth; // Width of parent container
+			var maxHeight = document.body.clientHeight; // Height of page
+			var ratio = 0; // Used for aspect ratio
+			var width = iframe.getAttribute("data-width"); // Original video width
+			var height = iframe.getAttribute("data-height"); // Original video height
+			// Check if the current width is larger than the max
+			if (width != maxWidth) {
+				ratio = maxWidth / width; // get ratio for scaling video
+				iframe.setAttribute("width", maxWidth);
+				iframe.setAttribute("height", height * ratio);
+				height = height * ratio; // Reset height to match scaled video
+				width = width * ratio; // Reset width to match scaled video
+			}
+			// Check if current height is larger than max
+			if (height > maxHeight) {
+				ratio = maxHeight / height; // Get ratio for scaling video
+				iframe.setAttribute("height", maxHeight);
+				iframe.setAttribute("width", width * ratio);
+				width = width * ratio; // Reset width to match scaled video
+				height = height * ratio; // Reset height to match scaled video
+			}
 		}
 	});
-});
+}
+
+// Resize videos on page load and after window is resized
+window.addEventListener("DOMContentLoaded", resizeVids);
+window.addEventListener("resize", resizeVids);
